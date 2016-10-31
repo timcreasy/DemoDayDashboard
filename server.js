ae01942d0bb06688453bbbdd6036a6f5447a572d
Tim Creasy <timcreasy@me.com>
2016-10-31 15:35:46 -0500

    Add cors

--- server.js
+++ server.js
@@ -9,7 +9,21 @@ const sess = {
 };
 
 const PORT = process.env.PORT || 3000;
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(session(sess));
 app.use(bodyParser.urlencoded({
     extended: true
